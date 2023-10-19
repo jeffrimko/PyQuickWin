248374ba49ff7b75ea8d6b7a36d17257f86ec93d
jeffrimko <jeffrimko@gmail.com>
2023-10-18 20:55:21 -0400

    Updates to help debug test issue.

--- tests/testlib.py
+++ tests/testlib.py
@@ -14,6 +14,7 @@ import unittest
 
 def _add_relative_dir_to_syspath(reldir):
     absdir = op.normpath(op.join(op.abspath(op.dirname(__file__)), reldir))
+    import os; print(os.listdir(absdir))
     sys.path.insert(0, absdir)
 
 ##==============================================================#
